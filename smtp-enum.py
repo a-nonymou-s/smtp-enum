6b58a8c36f911624b76bfde2ae9615a265106c45
Anonymous <72033787+yassinevirtual2@users.noreply.github.com>
2021-09-29 14:36:13 +0100

    Update smtp-enum.py

--- smtp-enum.py
+++ smtp-enum.py
@@ -1,19 +1,20 @@
-#!usr/bin/python
+#!usr/bin/python3
 import sys
-import sockets
+import socket
 if len(sys.argv) !=3 :
-	print"[*] Usage ./smtp-enum.py <ip> <user>"
+	print("[*] Usage ./smtp-enum.py <ip> <user file>")
 	exit(0)
-ip = sys.argv[1]
-user = sys.argv[2]
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+for file in sys.argv[2]:
+	ip = sys.argv[1]
+	user = file
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-connect = s.connect((ip,25))
-banner = s.recv(1024)
+	connect = s.connect((ip,25))
+	banner = s.recv(1024)
 
-print(banner)
+	print(banner)
 
-s.send("VRFY " + user +'\r\n')
-result = s.recv(1024)
-print result
-s.close()
+	s.send("VRFY " + user +'\r\n')
+	result = s.recv(1024)
+	print(result)
+	s.close()
